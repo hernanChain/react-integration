16562b1cacc7c5a19ded483ca4c1982f74108a1c
AriasPaez <ariaspaez.dev@gmail.com>
2021-03-13 12:18:47 -0500

    Step 10: Add VideoForm  and establish routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import VideoList from './components/Videos/VideoList';
+import VideoForm from './components/Videos/VideoForm';
 import "bootswatch/dist/pulse/bootstrap.min.css";
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,6 +15,8 @@ ReactDOM.render(
       <div className="container p-4">
         <Switch>
           <Route exact path="/" component={VideoList} />
+          <Route path="/new-video" component={VideoForm} />
+          <Route path="/update/:id" component={VideoForm} />
         </Switch>
         <ToastContainer/>
         </div>
