288d6d1f52f3a10eedcdd7a1fe8f4673638e4f8c
AriasPaez <ariaspaez.dev@gmail.com>
2021-03-13 11:32:16 -0500

    Step 5: Create VideosService for fetch API data

---
+++ src/components/Videos/VideosService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API = 'https://apirest-react-integration.herokuapp.com'
+export const loadVideos = async ()=>{
+    const getVideos = await axios.get(`${API}/getVideos`);
+    return getVideos;
+}
+
+export const createVideo = async (video)=>{
+    const createVideo = await axios.post(`${API}/createVideo`, video);
+    return createVideo;
+}
+
+export const getVideo = async (id) => {
+    const getVideo = await axios.get(`${API}/getVideo/${id}`)
+    return getVideo;
+}
+
+export const updateVideo = async (id, video) =>{
+    const updateVideo = await axios.patch(`${API}/updateVideo/${id}`,video)
+    return updateVideo;
+}
+
+export const deleteVideo = async (id) =>{
+    const deleteVideo = await axios.delete(`${API}/deleteVideo/${id}`)
+    return deleteVideo;
+}
